e314f99cff5ff8d93f69efa4553ce7f5652252a2
Godzilla <ereh_@naver.com>
2025-02-19 02:07:44 +0900

    fix

--- Backend/Composition_Object/MainStream/0_Router/0.5_DataBase_Router/database_router.js
+++ Backend/Composition_Object/MainStream/0_Router/0.5_DataBase_Router/database_router.js
@@ -16,35 +16,41 @@ export let Database_Router = class Router {
     constructor(){
 
     let MasterDB = new Pool({
-        connectionString: process.env.DATABASE_URI,
+        user: `${process.env.DATABASE_USER}`,
+        host: `${process.env.DATABASE_HOST}`,
+        database: `${process.env.DATABASE_ACCESSING}`,
+        password: `${process.env.DATABASE_PASSWORD}`,
+        port: `${process.env.DATABASE_PORT}`,
         max: 10,
         idleTimeoutMillis: 30000,
-        ssl: {
-            rejectUnauthorized: false
-          }
+        
         
     })
 
     let DB = new Pool({
-        connectionString: process.env.DATABASE_URI,
+        user: `${process.env.DATABASE_USER}`,
+        host: `${process.env.DATABASE_HOST}`,
+        database: `${process.env.DATABASE_ACCESSING}`,
+        password: `${process.env.DATABASE_PASSWORD}`,
+        port: `${process.env.DATABASE_PORT}`,
+
         max: 10,
         idleTimeoutMillis: 30000,
-        ssl: {
-            rejectUnauthorized: false
-          }
+        
         
     })
 
     let DB_usercreation = new Pool({
-        connectionString: process.env.DATABASE_URI,
+        user: `${process.env.DATABASE_USER}`,
+        host: `${process.env.DATABASE_HOST}`,
+        database: `${process.env.DATABASE_ACCESSING}`,
+        password: `${process.env.DATABASE_PASSWORD}`,
+        port: `${process.env.DATABASE_PORT}`,
         max: 10,
         idleTimeoutMillis: 30000,
-        ssl: {
-            rejectUnauthorized: false
-          }
+        
         
     })
-
     MasterDB.connect()
     DB.connect()
     DB_usercreation.connect()
