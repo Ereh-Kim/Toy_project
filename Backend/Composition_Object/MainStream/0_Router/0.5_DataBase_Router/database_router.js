ac1d3afc21c117d3c22cdef5cacc279bca1c606b
Godzilla <ereh_@naver.com>
2025-02-19 02:47:18 +0900

    fix

--- Backend/Composition_Object/MainStream/0_Router/0.5_DataBase_Router/database_router.js
+++ Backend/Composition_Object/MainStream/0_Router/0.5_DataBase_Router/database_router.js
@@ -17,33 +17,24 @@ export let Database_Router = class Router {
 
     let MasterDB = new Client({
         connectionString: process.env.DATABASE_URL,
-        max: 10,
-        idleTimeoutMillis: 30000,
-        ssl: {
-            rejectUnauthorized: false
-          }
+        
+        ssl: true
         
         
     })
 
     let DB = new Client({
         connectionString: process.env.DATABASE_URL,
-        max: 10,
-        idleTimeoutMillis: 30000,
-        ssl: {
-            rejectUnauthorized: false
-          }
+        
+        ssl: true
         
         
     })
 
     let DB_usercreation = new Client({
         connectionString: process.env.DATABASE_URL,
-        max: 10,
-        idleTimeoutMillis: 30000,
-        ssl: {
-            rejectUnauthorized: false
-          }
+        
+        ssl: true
         
         
     })
