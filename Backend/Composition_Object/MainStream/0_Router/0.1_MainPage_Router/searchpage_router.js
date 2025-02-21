41c2dce64f83d45c7ce8efc7f4a18d2c6d6fe529
Godzilla <ereh_@naver.com>
2025-02-21 17:22:40 +0900

    fix

--- Backend/Composition_Object/MainStream/0_Router/0.1_MainPage_Router/searchpage_router.js
+++ Backend/Composition_Object/MainStream/0_Router/0.1_MainPage_Router/searchpage_router.js
@@ -30,6 +30,13 @@ import { fileURLToPath } from 'url';
                 res.sendFile(ClientPage_path)
 
             })
+        
+            this.Pure_Router.get('/location/**',(req, res)=>{
+
+                const ClientPage_path = path.join(__dirname, '../../../../../Frontend/MainService_LocateSeacher_onprocess/build', 'index.html')
+                res.sendFile(ClientPage_path)
+
+            })
         }
     }
 
